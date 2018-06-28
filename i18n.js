4cf7d5c4d7d4bd1ff765c1cf0470cdcc7759660b
김화성 <hs.kim@albamapp.com>
2018-06-28 17:50:12 +0900

    locize에서 download latest 한 다음 translations로 최상위의 폴더명을 바꾼후 각 언어 폴더마다 index.js를 만들어 translationFormat 파일의 내용을 그대로 복붙하면 로컬에서 번역파일을 쓸 수 있게 함

--- i18n.js
+++ i18n.js
@@ -1,31 +1,37 @@
 import i18n from 'i18next';
-import LocizeBackend from 'i18next-locize-backend';
+// import LocizeBackend from 'i18next-locize-backend';
 import i18nextReactNative from 'i18next-react-native-language-detector';
 import { reactI18nextModule } from 'react-i18next';
+import * as en from './translations/en';
+import * as de from './translations/de';
+import * as ko from './translations/ko';
+
+console.log('en: ', en);
 
 i18n
-.use(LocizeBackend)
+// .use(LocizeBackend)
 .use(i18nextReactNative)
 .use(reactI18nextModule)
 .init({
   fallbackLng: 'en',
-  load: 'languageOnly',
-  whitelist: ['en', 'de', 'ko'],
+  resources: {
+    en, de, ko
+  },
   ns: ['common'],
   defaultNS: 'common',
 
-  debug: false,
+  debug: true,
   saveMissing: true,
 
   interpolation: {
     escapeValue: false, // not needed for react as it does escape per default to prevent xss!
   },
 
-  backend: {
+  /* backend: {
     referenceLng: 'en',
     apiKey: 'b1791d47-3b93-4b6b-ad66-a9d249ec7b45',
     projectId: 'fda8ac8e-2e52-47dd-a6b9-896cf479a703'
-  }
+  }*/
 });
 
 export default i18n;
---
+++ translations/en/index.js
@@ -0,0 +1,5 @@
+const common = require('./common.json');
+const home = require('./home.json');
+const page2 = require('./page2.json');
+
+export { common, home, page2 };
